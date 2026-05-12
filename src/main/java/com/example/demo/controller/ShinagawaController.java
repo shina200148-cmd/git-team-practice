c525ea00431b9a33e7773385718a824b721fefb1
takeru shinagawa <shina200148@gmail.com>
2026-05-12 15:22:56 +0900

    メッセージ編集

--- src/main/java/com/example/demo/controller/ShinagawaController.java
+++ src/main/java/com/example/demo/controller/ShinagawaController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ShinagawaController {
+	// チーム演習の練習
 	@GetMapping("/")
 	public String index() {
 		return "hello";
